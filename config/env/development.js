7fb17fe0fe031437c52d3566203a449856b15b1d
fullender <mdenyabut@yahoo.com>
2020-12-31 15:29:37 +0800

    add OAuth credentials in development config

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,20 @@ const devConfig = {
   jwt_expiration: 360000,
   dbConnectionString: `mongodb://127.0.0.1:27017/assetmanagement`,
   mongoDebug: true,
+  //nodemailer
   gmail_password: "zcxzgmskngzjcpog",
+  //oauth
+  SECRET_TOKEN: "b3assetmanagement",
+  SECRET_TOKEN_EXPIRED_IN: "24h",
+  REFRESH_SECRET_TOKEN: "b3assetmanagementsecretrefreshtokenkey",
+  REFRESH_SECRET_TOKEN_EXPIRED_IN: "30d",
+  GOOGLE_OAUTH_CLIENT_ID:
+    "327108575861-4e88u5hnp4ot23p9lisplpgfkgtvbhue.apps.googleusercontent.com",
+  GOOGLE_OAUTH_CLIENT_SECRET: "E0UX-xJfpmU00cttayRH2Ulu",
+  GOOGLE_OAUTH_CALLBACK: "http://localhost:5000/users/google/callback",
+  FACEBOOK_OAUTH_CLIENT_ID: "231302151732871",
+  FACEBOOK_OAUTH_CLIENT_SECRET: "e68ba7731ad16540b1bc65ae5e716429",
+  FACEBOOK_OAUTH_CALLBACK: "http://localhost:5000/users/facebook/callback",
 };
 
 export default devConfig;
